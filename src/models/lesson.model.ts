3e419eed008397ca5d15e6a45e5657cb49a94696
John-JuniorFofie <johnfofie31@gmail.com>
2026-04-06 09:55:04 +0000

    modified:   src/models/lesson.model.ts

--- src/models/lesson.model.ts
+++ src/models/lesson.model.ts
@@ -11,6 +11,32 @@ const lessonSchema = new mongoose.Schema({
   videoUrl: String,
   duration: Number, 
   order: Number,
+
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  lessonId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lesson",
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  score: Number,
+  completedAt: Date,
+  type:{
+    type: String,
+    enum: ["lesson", "assignment"],
+  },
+  total: Number,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 }, { timestamps: true });
 
 export default mongoose.model("Lesson", lessonSchema);
