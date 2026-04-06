77621550d2df5b4343ac3ee5687ae44630b2c3c7
John-JuniorFofie <johnfofie31@gmail.com>
2026-04-06 09:43:30 +0000

    new file:   src/controllers/lesson.controller.ts 	new file:   src/models/lesson.model.ts

---
+++ src/models/lesson.model.ts
@@ -0,0 +1,16 @@
+
+import mongoose from "mongoose";
+
+const lessonSchema = new mongoose.Schema({
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  title: String,
+  videoUrl: String,
+  duration: Number, 
+  order: Number,
+}, { timestamps: true });
+
+export default mongoose.model("Lesson", lessonSchema);
